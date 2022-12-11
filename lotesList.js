aef266231a30e08564b3bd8c2a4575f1f90327fd
dxwebster <adrianalimafm@gmail.com>
2022-12-11 20:50:52 -0300

    refactor index html

---
+++ lotesList.js
@@ -0,0 +1,8 @@
+var lotesList = [
+  ["GM1050", "GM800", "GP400A"],
+  ["GP750", "GS600"],
+  ["GM550", "GM600", "GM650"],
+  // ["GM500", "GM500G", "GM600G"],
+]
+
+module.exports = lotesList
--- scripts.sh
+++ scripts.sh
@@ -7,26 +7,33 @@ HTML_TO=$BUILD_PATH'/index.html'
 IMG_FROM='src/'$PRODUCT_NAME'/img'
 IMG_TO=$BUILD_PATH
 
-IMG_COMMON_FROM='src/common/img'
-IMG_COMMON_TO=$BUILD_PATH
-
 FONTS_FROM='src/common/fonts'
 FONTS_TO=$BUILD_PATH'/fonts'
 
-CSS_FROM='src/common/css/main.css'
-CSS_TO=$BUILD_PATH'/css/main.css'
+CSS_COMOM_FROM='src/common/styles/main.css'
+CSS_COMMON_TO=$BUILD_PATH'/styles/main.css'
 
+CSS_CUSTOM_FROM='src/'$PRODUCT_NAME'/styles/custom.css'
+CSS_CUSTOM_TO=$BUILD_PATH'/styles/custom.css'
 
 build_project(){
   
-  echo $PRODUCT_NAME
+  echo $PRODUCT_NAME '-------------'
   
-  # Minify CSS
-  cleancss -o $CSS_TO $CSS_FROM 
+  # Minify Common CSS
+  cleancss -o $CSS_COMMON_TO $CSS_COMOM_FROM 
   if [ $? -eq 0 ]; then
-    echo '✔ CSS minified'
+    echo '✔ Common CSS minified'
   else
-    echo '❌ CSS not minified'
+    echo '❌ Common CSS not minified'
+  fi
+
+  # Copy Custom Style CSS
+  cp -r $CSS_CUSTOM_FROM $CSS_CUSTOM_TO
+  if [ $? -eq 0 ]; then
+    echo '✔ Custom CSS copied'
+  else
+    echo '❌ Custom CSS not copied'
   fi
 
   # Copy IMAGES
@@ -36,16 +43,7 @@ build_project(){
   else
     echo '❌ Images not copied'
   fi
-
-  # Copy Common IMAGES
-  cp -r $IMG_COMMON_FROM $IMG_COMMON_TO  
-  if [ $? -eq 0 ]; then
-    echo '✔ Common Images copied'
-  else
-    echo '❌ Common Images not copied'
-  fi
     
-  
   # Copy FONTS
   cp -r $FONTS_FROM $FONTS_TO 
   if [ $? -eq 0 ]; then
@@ -62,14 +60,33 @@ build_project(){
     echo '❌ HTML not minified'
   fi
 
-  # replace css url
-  OLD='\.\.\/common\/css\/main\.\css'
-  NEW='css\/main\.\css'
-  sed -i.bak "s/$OLD/$NEW/g" $BUILD_PATH'/index.html'
+  # replace commom css url
+  OLD_COMMON='\.\.\/common\/styles\/main\.\css'
+  NEW_COMMON='styles\/main\.\css'
+  sed -i.bak "s/$OLD_COMMON/$NEW_COMMON/g" $BUILD_PATH'/index.html'
   if [ $? -eq 0 ]; then
-    echo '✔ fix CSS URL in HTML'
+    echo '✔ fix COMMOM CSS URL in HTML'
   else
-    echo '❌ CSS URL not fixed'
+    echo '❌ COMMOM CSS URL not fixed'
+  fi
+
+  # replace custom css url
+  OLD_CUSTOM='\styles\/custom\.\css'
+  NEW_CUSTOM='styles\/custom\.\css'
+  sed -i.bak "s/$OLD_CUSTOM/$NEW_CUSTOM/g" $BUILD_PATH'/index.html'
+  if [ $? -eq 0 ]; then
+    echo '✔ fix CUSTOM CSS URL in HTML'
+  else
+    echo '❌ CUSTOM CSS URL not fixed'
+  fi
+
+  #  zip project
+  cd build
+  zip -r $PRODUCT_NAME'.zip' $PRODUCT_NAME
+  if [ $? -eq 0 ]; then
+    echo '✔ zip folder'
+  else
+    echo '❌ folder not ziped'
   fi
 }
 
