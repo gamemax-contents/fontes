74232f71a7151da07865084b76cb1f38a7c7a9fd
dxwebster <adrianalimafm@gmail.com>
2022-12-11 21:48:15 -0300

    add GM500 in index

--- lotesList.js
+++ lotesList.js
@@ -2,7 +2,10 @@ var lotesList = [
   ["GM1050", "GM800", "GP400A"],
   ["GP750", "GS600"],
   ["GM550", "GM600", "GM650"],
-  // ["GM500", "GM500G", "GM600G"],
+  [
+    "GM500",
+    //"GM500G", "GM600G"
+  ],
 ]
 
 module.exports = lotesList
