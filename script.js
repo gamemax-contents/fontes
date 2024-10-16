191afbb23a54b9f5217917273b35b450f583db5f
dxwebster <adrianalimafm@gmail.com>
2024-10-16 16:05:43 -0300

    add download button

---
+++ script.js
@@ -0,0 +1,48 @@
+// Lista de produtos
+var list = [
+  [
+    "TL900",
+    "TL1050",
+    "GM1050",
+    "GM800",
+    "GP400A",
+    "GP750",
+    "GS600",
+    "GM550",
+    "GM600",
+    "GM650",
+    "GM500",
+  ],
+];
+
+// Função para criar item da lista
+function item(name) {
+  return `
+      <li>
+        <span>${name}</span>
+        <a href="build/${name}" class="ver" target="_blank">ver</a>
+        <button onclick="download('${name}')">Download</button>
+      </li>
+    `;
+}
+
+const mainContent = list.map((lote) => {
+  return `
+      <form>
+        <fieldset>
+          ${lote.map((name) => item(name)).join("")}
+        </fieldset>
+      </form>
+    `;
+});
+
+function download(name) {
+  const link = document.createElement("a");
+  link.href = `build/${name}.zip`;
+  link.download = name;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+document.querySelector("main").innerHTML = mainContent.join("");
